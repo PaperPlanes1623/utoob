6b9818898729b95d0ca518241a7ffcf6f8c06b74
Dylan Taft <dylbronjames@gmail.com>
2019-06-01 10:43:44 -0600

    controllers updated

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,40 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :delete]
   def index
+    @movies = Movie.all
   end
 
   def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new
+    end
   end
 
   def show
   end
 
+  def edit
+  end
+
   def delete
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :duration, :genre, :description, :trailer)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,40 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :delete]
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
   end
 
+  def edit
+  end
+
   def delete
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
