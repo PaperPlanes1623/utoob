c54530e1900800de499cd986b62931bbe1596a14
Tyson Hyland <tyhyland@hotmail.com>
2019-06-01 16:02:21 -0600

    minor change to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   root 'users#index'
-<<<<<<< HEAD
-
-  resources :users
-
-  resources :movies do
-    resources :comments    
-=======
   resources :users 
 
   resources :movies do
     resources :comments
->>>>>>> 62cfe12c0a5d9c41b197d92e26456787cba73688
   end
 end
